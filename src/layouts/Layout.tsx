6be875ef0294a17631b2644d814683f6332f3694
Miguel Rivera <miguel.sky.215@gmail.com>
2020-07-23 12:45:46 -0700

    Improve layout using flex

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,21 +1,48 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import Box from '@material-ui/core/Box'
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 
 import Home from '../pages/Home'
 import Header from './Header'
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      position: 'absolute',
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0,
+
+      display: 'flex',
+      flexDirection: 'column',
+    },
+
+    content: {
+      flex: '1 1 0%',
+      display: 'flex',
+      flexDirection: 'column',
+
+      overflowX: 'hidden',
+      overflowY: 'auto',
+      padding: theme.spacing(2),
+    },
+  })
+)
+
 function Layout() {
+  const classes = useStyles()
+
   return (
-    <>
+    <div className={classes.root}>
       <Header />
 
-      <Box m={2}>
+      <div className={classes.content}>
         <Switch>
           <Route exact path="/" component={Home} />
         </Switch>
-      </Box>
-    </>
+      </div>
+    </div>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Box from '@material-ui/core/Box'
 import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
 interface State {
   pickupAddress: string
@@ -23,42 +24,58 @@ export default function Home() {
     setValues({ ...values, [prop]: event.target.value })
   }
 
+  const handleSubmit = (event: React.MouseEvent) => {}
+
   return (
-    <div>
-      <Box mb={2}>
-        <TextField
-          fullWidth
-          label="Pickup Address"
-          onChange={handleChange('pickupAddress')}
-        />
+    <Box flexGrow={1} display="flex" flexDirection="column">
+      <Box flexGrow={1}>
+        <Box mb={2}>
+          <TextField
+            fullWidth
+            label="Pickup Address"
+            onChange={handleChange('pickupAddress')}
+          />
+        </Box>
+
+        <Box mb={2}>
+          <TextField
+            fullWidth
+            label="Delivery Address"
+            onChange={handleChange('deliveryAddress')}
+          />
+        </Box>
+
+        <Box mb={2}>
+          <TextField
+            fullWidth
+            label="Job Name"
+            onChange={handleChange('jobName')}
+          />
+        </Box>
+
+        <Box mb={2}>
+          <TextField
+            fullWidth
+            label="Latest Deliver By"
+            type="datetime-local"
+            defaultValue="2017-05-24T10:30"
+            InputLabelProps={{ shrink: true }}
+            onChange={handleChange('lastestDeliverByTime')}
+          />
+        </Box>
       </Box>
 
-      <Box mb={2}>
-        <TextField
+      <Box>
+        <Button
           fullWidth
-          label="Delivery Address"
-          onChange={handleChange('deliveryAddress')}
-        />
+          variant="contained"
+          color="primary"
+          size="large"
+          onClick={handleSubmit}
+        >
+          Continue
+        </Button>
       </Box>
-
-      <Box mb={2}>
-        <TextField
-          fullWidth
-          label="Job Name"
-          onChange={handleChange('jobName')}
-        />
-      </Box>
-
-      <Box mb={2}>
-        <TextField
-          fullWidth
-          label="Latest Deliver By"
-          type="datetime-local"
-          defaultValue="2017-05-24T10:30"
-          InputLabelProps={{ shrink: true }}
-          onChange={handleChange('lastestDeliverByTime')}
-        />
-      </Box>
-    </div>
+    </Box>
   )
 }
