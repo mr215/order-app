5766cc84c72e1c6ea1ab5bcc1c8cb8e3c3f13052
Miguel Rivera <miguel.sky.215@gmail.com>
2020-07-24 10:26:41 -0700

    Add OrderForm

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,35 +1,32 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
 
 import Home from '../pages/Home'
 import Header from './Header'
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      position: 'absolute',
-      top: 0,
-      right: 0,
-      bottom: 0,
-      left: 0,
+const useStyles = makeStyles({
+  root: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
 
-      display: 'flex',
-      flexDirection: 'column',
-    },
+    display: 'flex',
+    flexDirection: 'column',
+  },
 
-    content: {
-      flex: '1 1 0%',
-      display: 'flex',
-      flexDirection: 'column',
+  content: {
+    flex: '1 1 0%',
+    display: 'flex',
+    flexDirection: 'column',
 
-      overflowX: 'hidden',
-      overflowY: 'auto',
-      padding: theme.spacing(2),
-    },
-  })
-)
+    overflowX: 'hidden',
+    overflowY: 'auto',
+  },
+})
 
 function Layout() {
   const classes = useStyles()
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,9 @@
-import React from 'react'
-import Box from '@material-ui/core/Box'
-import TextField from '@material-ui/core/TextField'
-import Button from '@material-ui/core/Button'
-import Radio from '@material-ui/core/Radio'
-import RadioGroup from '@material-ui/core/RadioGroup'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
-import FormControl from '@material-ui/core/FormControl'
-import FormLabel from '@material-ui/core/FormLabel'
+import React, { useState } from 'react'
 
-interface State {
-  pickupAddress: string
-  deliveryAddress: string
-  vehicleType: string
-  lastestDeliverByTime: string
-  jobName: string
-  orderThrough: string
-}
+import OrderForm, { OrderFormValues } from '../components/forms/OrderForm'
 
 export default function Home() {
-  const [values, setValues] = React.useState<State>({
+  const [values, setValues] = useState<OrderFormValues>({
     pickupAddress: '',
     deliveryAddress: '',
     vehicleType: 'car',
@@ -27,111 +12,9 @@ export default function Home() {
     orderThrough: '',
   })
 
-  const handleChange = (prop: keyof State) => (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setValues({ ...values, [prop]: event.target.value })
+  const handleSubmit = (values: OrderFormValues) => {
+    // TODO:
   }
 
-  const handleSubmit = (event: React.MouseEvent) => {}
-
-  console.log('values', values)
-
-  return (
-    <Box flexGrow={1} display="flex" flexDirection="column">
-      <Box flexGrow={1}>
-        <Box mb={3}>
-          <TextField
-            fullWidth
-            label="Pickup Address"
-            value={values.pickupAddress}
-            onChange={handleChange('pickupAddress')}
-          />
-        </Box>
-
-        <Box mb={3}>
-          <TextField
-            fullWidth
-            label="Delivery Address"
-            value={values.deliveryAddress}
-            onChange={handleChange('deliveryAddress')}
-          />
-        </Box>
-
-        <Box mb={3}>
-          <FormControl component="fieldset">
-            <FormLabel component="legend">Vehicle Type</FormLabel>
-
-            <RadioGroup
-              row
-              aria-label="vehicleType"
-              name="vehicleType"
-              value={values.vehicleType}
-              onChange={handleChange('vehicleType')}
-            >
-              <FormControlLabel value="car" control={<Radio />} label="Car" />
-              <FormControlLabel
-                value="truck"
-                control={<Radio />}
-                label="Truck"
-              />
-            </RadioGroup>
-          </FormControl>
-        </Box>
-
-        <Box mb={3}>
-          <TextField
-            fullWidth
-            label="Latest Deliver By"
-            type="datetime-local"
-            value={values.lastestDeliverByTime}
-            InputLabelProps={{ shrink: true }}
-            onChange={handleChange('lastestDeliverByTime')}
-          />
-        </Box>
-
-        <Box mb={3}>
-          <TextField
-            fullWidth
-            label="Job Name"
-            value={values.jobName}
-            onChange={handleChange('jobName')}
-          />
-        </Box>
-
-        <Box mb={3}>
-          <FormControl component="fieldset">
-            <FormLabel component="legend">Order through SupplyHound?</FormLabel>
-
-            <RadioGroup
-              row
-              aria-label="orderThrough"
-              name="orderThrough"
-              value={values.orderThrough}
-              onChange={handleChange('orderThrough')}
-            >
-              <FormControlLabel
-                value="SupplyHound"
-                control={<Radio />}
-                label="Yes"
-              />
-              <FormControlLabel value="" control={<Radio />} label="No" />
-            </RadioGroup>
-          </FormControl>
-        </Box>
-      </Box>
-
-      <Box>
-        <Button
-          fullWidth
-          variant="contained"
-          color="primary"
-          size="large"
-          onClick={handleSubmit}
-        >
-          Continue
-        </Button>
-      </Box>
-    </Box>
-  )
+  return <OrderForm defaultValues={values} onSubmit={handleSubmit} />
 }
