83de60604d3063f1ebcb83b0c1e1cb102ce8f412
Miguel Rivera <miguel.sky.215@gmail.com>
2020-08-03 16:32:33 -0700

    Improve navigation between app pages

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
+import { useLocation, useHistory } from 'react-router-dom'
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
+import ArrowBackOutlinedIcon from '@material-ui/icons/ArrowBackOutlined'
 
 import SideMenu from './SideMenu'
 import logoImg from '../images/logo.png'
@@ -30,6 +32,9 @@ const useStyles = makeStyles((theme: Theme) =>
 )
 
 function Header() {
+  const location = useLocation()
+  const history = useHistory()
+
   const classes = useStyles()
   const [openSideMenu, setOpenSideMenu] = useState(false)
 
@@ -41,18 +46,33 @@ function Header() {
     setOpenSideMenu(open)
   }
 
+  const handleGoBack = () => {
+    history.goBack()
+  }
+
   return (
     <>
       <AppBar position="static" color="transparent">
         <Toolbar className={classes.toolbar}>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            onClick={handleMenuOpen}
-          >
-            <MenuIcon />
-          </IconButton>
+          {location.pathname === '/' ? (
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={handleMenuOpen}
+            >
+              <MenuIcon />
+            </IconButton>
+          ) : (
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="back"
+              onClick={handleGoBack}
+            >
+              <ArrowBackOutlinedIcon />
+            </IconButton>
+          )}
 
           <div className={classes.imageContainer}>
             <img src={logoImg} alt="logo" width={50} />
