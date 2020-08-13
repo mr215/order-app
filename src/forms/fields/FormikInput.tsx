e9d223f50389b98b89bb897aa320d1a539920585
Miguel Rivera <miguel.sky.215@gmail.com>
2020-08-13 11:25:23 -0700

    Improve FormikInput styles for error

--- src/forms/fields/FormikInput.tsx
+++ src/forms/fields/FormikInput.tsx
@@ -1,15 +1,19 @@
 import React, { ComponentProps } from 'react'
 import styled from 'styled-components'
+import clsx from 'clsx'
 import { FieldProps, getIn } from 'formik'
-import { IonItem, IonLabel, IonInput } from '@ionic/react'
+import { IonItem, IonLabel, IonInput, IonText } from '@ionic/react'
 
 interface InputProps extends ComponentProps<typeof IonInput> {
   label: string
   labelProps: ComponentProps<typeof IonLabel>
+  required?: boolean
 }
 
 const ErrorLabel = styled(IonLabel)`
-  transform: scale(0.75) !important;
+  padding: 0.25rem 1rem;
+  font-size: 0.75rem;
+  color: var(--ion-color-danger);
 `
 
 const FormikInput: React.FC<FieldProps & InputProps> = ({
@@ -17,33 +21,31 @@ const FormikInput: React.FC<FieldProps & InputProps> = ({
   form,
   label,
   labelProps,
+  required,
   ...props
 }) => {
   const error = getIn(form.errors, name)
   const touched = getIn(form.touched, name)
-  const labelColor = error && touched ? 'danger' : ''
 
   return (
-    <IonItem>
-      {label && (
-        <IonLabel {...labelProps} color={labelColor}>
-          {label}
-        </IonLabel>
-      )}
+    <>
+      <IonItem className={clsx({ 'ion-invalid': error && touched })}>
+        {label && (
+          <IonLabel {...labelProps} color={error && touched ? 'danger' : ''}>
+            {label} {required && <IonText color="danger">*</IonText>}
+          </IonLabel>
+        )}
 
-      <IonInput
-        {...props}
-        value={value}
-        onIonBlur={e => form.setFieldTouched(name)}
-        onIonChange={e => form.setFieldValue(name, e.detail.value)}
-      />
+        <IonInput
+          {...props}
+          value={value}
+          onIonBlur={e => form.setFieldTouched(name)}
+          onIonChange={e => form.setFieldValue(name, e.detail.value)}
+        />
+      </IonItem>
 
-      {error && touched && (
-        <ErrorLabel position="stacked" color={labelColor}>
-          {error}
-        </ErrorLabel>
-      )}
-    </IonItem>
+      {error && touched && <ErrorLabel>{error}</ErrorLabel>}
+    </>
   )
 }
 
