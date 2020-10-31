905199f975097604ee551a7b02fec84153d062c0
Miguel Rivera <miguel.sky.215@gmail.com>
2020-10-31 16:36:58 -0700

    Remove the part of loading data in Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,17 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { RouteComponentProps } from 'react-router-dom'
-import { IonLoading, IonPage, IonToast } from '@ionic/react'
+import { IonPage } from '@ionic/react'
 import { toJS } from 'mobx'
 import { observer } from 'mobx-react-lite'
 
-import { TOAST_DURATION } from 'utils/config'
 import { MainOrderFormValues } from 'types'
 import useStores from 'hooks/useStores'
-import { fetchProfile, fetchPaymentMethods } from 'utils/api'
 
 import Header from 'components/Header'
 import MainOrderForm from 'forms/MainOrderForm'
 
 const Home: React.FC<RouteComponentProps> = ({ history }) => {
   const { appStore, orderStore } = useStores()
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState('')
 
   const handleSubmit = (values: MainOrderFormValues) => {
     orderStore.updateOrder(values)
@@ -24,41 +20,13 @@ const Home: React.FC<RouteComponentProps> = ({ history }) => {
   }
 
   useEffect(() => {
-    const loadData = async () => {
-      try {
-        setLoading(true)
-
-        const [profileResponse, paymentMethodsResponse] = await Promise.all([
-          fetchProfile(),
-          fetchPaymentMethods(),
-        ])
-
-        appStore.setProfile(profileResponse.data.data)
-        appStore.setPaymentMethods(paymentMethodsResponse.data.data)
-
-        // If payment is not setup, redirect to payment-setup page
-        if (!appStore.paymentMethods.length) {
-          history.push({ pathname: '/payment-setup' })
-        }
-      } catch (e) {
-        setError(e.toString())
-      } finally {
-        setLoading(false)
-      }
-    }
-
-    // Load data
-    if (!appStore.profile) {
-      loadData()
+    if (!appStore.paymentMethods.length) {
+      history.push({ pathname: '/payment-setup' })
     }
   }, [])
 
   return (
     <IonPage>
-      <IonLoading isOpen={loading} />
-
-      {error && <IonToast isOpen message={error} duration={TOAST_DURATION} />}
-
       <Header home />
 
       <MainOrderForm order={toJS(orderStore.order)} onSubmit={handleSubmit} />
